40aaf68a9cc7aede21b2cc4081b193a1c411abba
Gael Sauzet <sauzetgael@gmail.com>
2018-09-17 22:24:20 +0900

    POM-1 : Added user Model

---
+++ TaskerApi/Interfaces/IUserService.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TaskerApi.Models.Oauth;
+
+namespace TaskerApi.Interfaces
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IUserService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        Task<User> Authenticate(string username, string password);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<User> GetAll();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<User> GetById(string id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        Task<User> Create(User user, string password);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        Task<bool> Update(User user, string password = null);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<bool> Delete(string id);
+    }
+}
---
+++ TaskerApi/TaskerDbContext/TaskerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskerApi.Models.Oauth;
+
+namespace TaskerApi.TaskerDbContext
+{
+    public class TaskerContext : DbContext
+    {
+        public TaskerContext(DbContextOptions<TaskerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
